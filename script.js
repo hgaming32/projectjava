6399556e524f87681a7a4082d61657fbbe384fe5
hgaming32 <alkimaabdi666@gmail.com>
2024-09-02 04:26:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let studentInfo = () =>  {
+  return [
+
+    {
+        ID : 1,
+        Name : "Abdullahi", 
+        age : 13, 
+        gpa : 4.3    
+    },
+
+    {
+        ID : 2,
+        Name : "Mohammed", 
+        age : 13, 
+        gpa : 3.1
+    },
+
+    {
+        ID : 3,
+        Name : "Jibril", 
+        age : 18,
+        gpa : 4.9
+    }
+
+
+
+  ]
+};
+
+studentInfo();
